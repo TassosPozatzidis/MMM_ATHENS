d3b109036d0c61ff65b702d2434fbd2169eb960d
Tassos.Pozatzidis <anastasiospoz@gmail.com>
2021-12-22 12:30:15 +0200

    this is mi first commit

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+// const cors = require('cors')({
+// origin: true,
+// });
+
+// http request 1
+exports.randomNumber=functions.https.onRequest((request, response)=>{
+  const number = Math.round(Math.random() * 100);
+  console.log(number);
+  response.send(number.toString());
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send({
+    first_name: "Andreas",
+    last_name: "Menychtas",
+    registered: "true",
+  });
+});
+
+// http request 1
+exports.toHome=functions.https.onRequest((request, response)=>{
+  response.redirect("https://mmm-oasa.web.app/generic.html");
+});
+
+// http callable function
+exports.sayHello = functions.https.onCall((data, context)=>{
+  return "hello, all";
+});
